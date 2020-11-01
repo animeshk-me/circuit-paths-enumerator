d7741e8feea20d8a853e2fd860eac28107de88d5
Animesh241100 <24animesh11@gmail.com>
2020-11-01 16:42:34 +0530

    Update input.v to bring in the required format

--- input.v
+++ input.v
@@ -6,11 +6,11 @@ output N22,N23;
 
 wire N10,N11,N16,N19;
 
-or OR2_1 (N10, N1, N3);
-nand NAND2_2 (N11, N3, N6);
-nand NAND2_3 (N16, N2, N11);
-nand NAND2_4 (N19, N11, N7);
-nand NAND2_5 (N22, N10, N16);
-nand NAND2_6 (N23, N16, N19);
+or OR2_1(N10,N1,N3);
+nand NAND2_2(N11,N3,N6);
+nand NAND2_3(N16,N2,N11);
+nand NAND2_4(N19,N11,N7);
+nand NAND2_5(N22,N10,N16);
+nand NAND2_6(N23,N16,N19);
 
 endmodule
