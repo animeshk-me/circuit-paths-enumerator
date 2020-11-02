385062e60d5cd1bd89b40bea53437f95ee40b6cd
Animesh241100 <24animesh11@gmail.com>
2020-11-02 16:10:21 +0530

    Add comments in classes

--- classes.py
+++ classes.py
@@ -1,18 +1,20 @@
 from collections import defaultdict
 
+#***************************************************************#
+
+# A Gate class to package all the data of a gate
 class Gate:
-  
   def __init__(self, name, inputs, output):
     self.name = name
-    self.inputs = inputs
-    self.output = output
+    self.inputs = inputs  # list of inputs
+    self.output = output  # only one output(primitive gates)
 
+#***************************************************************#
 
-
-
+# A Graph class to represent a graph using adjancency list
 class Graph:
   def __init__(self):
-    self.adj = defaultdict(list)
+    self.adj = defaultdict(list) # Adjacency list
   
   # Adds a directed edge from a to b (a---->b)
   def add_edge(self, a, b):
@@ -20,11 +22,9 @@ class Graph:
 
   # prints the graph
   def print_graph(self, name):
-    print(f"The graph {name}:\n_________________________________")
+    print(f"The graph {name}:\n----------------------------")
     for vertex in self.adj:
       print(vertex, "--->", self.adj[vertex])
-    print("_________________________________\n");
+    print("-----------------------------------\n");
 
-
-# class Node:
-#   def __init__(self, name):
+#***************************************************************#
