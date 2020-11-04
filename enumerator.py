d50b701f7e55ebcf35558056b985bcfbba76093f
Animesh241100 <24animesh11@gmail.com>
2020-11-04 09:09:35 +0530

    Update printing format

--- enumerator.py
+++ enumerator.py
@@ -1,3 +1,9 @@
+#*****************************************************************#
+'''
+The Main Module which drives all the code.
+'''
+#*****************************************************************#
+
 from parser import (
   get_input_list,
   get_output_list,
@@ -21,25 +27,24 @@ def main():
   # convert the circuit I/O info into python list
   input_list = get_input_list(fp)
   output_list = get_output_list(fp)
-  print("Overall inputs", input_list)
-  print("Overall outputs", output_list)
   
   # convert the cicuit gates into
-  gate_data = get_gates_data(fp);
-  for gate in gate_data:
-    print(gate.name, gate.inputs, gate.output)
+  gate_data = get_gates_data(fp)
   fp.close() # close files
   
   # converting the data into a graph
   final_graph = get_final_graph(gate_data, input_list, output_list)  
   final_graph.print_graph("Final")
 
+  print("All paths from inputs to outputs:\n")
   # enumerate all possible path from Inputs to outputs and print
   all_paths = get_final_paths(input_list, output_list, final_graph)
   print(*all_paths, sep="\n")
+  print("Total number of paths:", len(all_paths))
 
 
 # Entry point
 if __name__ == "__main__":
   main()
  
+#*****************************************************************#
